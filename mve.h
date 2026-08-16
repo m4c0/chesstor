3c1977d1e8563fa56ba76944567970b4df123754
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-16 12:35:49 +0100

    mve: pawn is kinda complex

--- mve.h
+++ mve.h
@@ -9,21 +9,70 @@ enum mve_piece_type_e {
   mve_p_bish,
   mve_p_quen,
   mve_p_king,
+  mve_p_errd = 0xF,
 };
 int mve_is_valid(unsigned * board, int from, int to);
 
 #ifdef MVE_IMPL
 
-int mve_is_valid(unsigned * board, int from, int to) {
-  int brd = board[from];
-  // int blk = 0; // TODO: differ logic to black pieces
+#define P(x) ((x) & 0xF)
+#define D(x) ((x) & 0x80)
 
-  int m = to - from;
-  switch (brd & 0xF) {
-    case mve_p_pawn:
-      if (m == -8) return 1;
-      if (m == -16 && (from / 8 == 6)) return 1;
-      return 0;
+typedef struct mve_s {
+  unsigned * board;
+  unsigned piece;
+  int from_x, from_y;
+  int to_x, to_y;
+  int dx, dy;
+  int dir;
+} mve_t;
+
+static int mve_piece_after_delta(const mve_t * mve, int dx, int dy) {
+  int x = mve->from_x + dx;
+  if (x < 0 || x > 7) return mve_p_errd;
+  int y = mve->from_y + dy;
+  if (y < 0 || y > 7) return mve_p_errd;
+  return mve->board[y * 8 + x];
+}
+
+static int mve_pawn_is_valid(const mve_t * mve) {
+  if (mve->dx == 0 && mve->dy == mve->dir) {
+    int b = mve_piece_after_delta(mve, 0, mve->dir);
+    if (P(b) == mve_p_none) return 1;
+    return 0;
+  }
+  if (mve->dx == 0 && mve->dy == 2 * mve->dir) {
+    if (mve->dir == -1 && mve->from_y != 6) return 0;
+    if (mve->dir ==  1 && mve->from_y != 1) return 0;
+    if (P(mve_piece_after_delta(mve, 0, mve->dir)) != mve_p_none) return 0;
+    int b = mve_piece_after_delta(mve, 0, mve->dir * 2);
+    if (P(b) == mve_p_none) return 1;
+    return 1;
+  }
+  if ((mve->dx == 1 || mve->dx == -1) && mve->dy == mve->dir) {
+    int b = mve_piece_after_delta(mve, 0, mve->dir);
+    if (P(b) == mve_p_none) return 0;
+    if (D(b) != D(mve->piece)) return 1;
+    return 0;
+  }
+  return 0;
+}
+
+int mve_is_valid(unsigned * board, int from, int to) {
+  mve_t mve  = {
+    .board   = board,
+    .piece   = board[from],
+    .from_x  = from % 8,
+    .from_y  = from / 8,
+    .to_x    = to % 8,
+    .to_y    = to / 8,
+  };
+  mve.dx  = mve.to_x - mve.from_x;
+  mve.dy  = mve.to_y - mve.from_y;
+  mve.dir = D(mve.piece) ? -1 : 1;
+
+  switch (P(mve.piece)) {
+    case mve_p_pawn: return mve_pawn_is_valid(&mve);
     case mve_p_towr:
     case mve_p_knit:
     case mve_p_bish:
