e1ca8bf9f31b3ff14e266f894db5b76343ee00f9
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-26 19:06:16 +0100

    bited: configurable atlas size and start

--- bited.h
+++ bited.h
@@ -1,38 +1,43 @@
 #ifndef BITED_H
 #define BITED_H
 
+#define BTD_W 256
+#define BTD_H 128
+
 typedef struct btd_upc {
   int x, y;
 } btd_upc_t;
 static btd_upc_t btd_pc;
 
-static uint8_t btd_atlas[128 * 32];
+static uint8_t btd_atlas[BTD_W * BTD_H];
 
 void btd_replace_atlas();
 
 void btd_cursor(int dx, int dy) {
   int x = btd_pc.x + dx;
-  if (x >= 0 && x < 128) btd_pc.x = x;
+  if (x >= 0 && x < BTD_W) btd_pc.x = x;
 
   int y = btd_pc.y + dy;
-  if (y >= 0 && y < 128) btd_pc.y = y;
+  if (y >= 0 && y < BTD_H) btd_pc.y = y;
 }
 
 void btd_toggle() {
-  int i = btd_pc.y * 128 + btd_pc.x;
+  int i = btd_pc.y * BTD_W + btd_pc.x;
   btd_atlas[i] = btd_atlas[i] ? 0 : 255;
   btd_replace_atlas();
 }
 
 void btd_load() {
   FILE * f = fopen("atlas.img", "rb");
-  fread(btd_atlas, 128 * 32, 1, f);
-  fclose(f);
+  if (f) {
+    fread(btd_atlas, BTD_W * BTD_H, 1, f);
+    fclose(f);
+  }
   btd_replace_atlas();
 }
 void btd_save() {
   FILE * f = fopen("atlas.img", "wb");
-  fwrite(btd_atlas, 128 * 32, 1, f);
+  fwrite(btd_atlas, BTD_W * BTD_H, 1, f);
   fclose(f);
 }
 
