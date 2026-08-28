de0500e3b0176ae11f711d43f870d14dd7ca4d48
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-28 18:49:44 +0100

    glu: trying to inline PC

--- glu.h
+++ glu.h
@@ -16,29 +16,31 @@ typedef struct glu_upc_s {
   unsigned hover;
   unsigned pick;
 } glu_upc_t;
-static glu_upc_t glu_pc;
 
 static int glu_scr_w;
 static int glu_scr_h;
 
-void g3d_resize(unsigned w, unsigned h) {
-  float a = (float)w / (float)h;
-  glu_pc.aspect_x = a > 1 ? a : 1;
-  glu_pc.aspect_y = a > 1 ? 1 : (1.0 / a);
+static g3d_buffer_t   * glu_upc;
+static g3d_pipeline_t * glu_ppl;
 
+void g3d_resize(unsigned w, unsigned h) {
   glu_scr_w = w;
   glu_scr_h = h;
 }
 
-int g3d_init(const g3d_api_t * t) {
+int g3d_init(const g3d_api_t * api) {
   srand(time(NULL));
 
   sfx_init();
   snd_init(sfx_filler);
 
-  g3d_resize(t->scr_w, t->scr_h);
+  g3d_resize(api->scr_w, api->scr_h);
   gme_reset();
 
+  glu_upc = api->new_buffer(api->ptr, sizeof(glu_upc_t));
+  glu_ppl = api->new_pipeline(api->ptr, "shader", 2, 0);
+
+  if (!glu_ppl) return 1;
   return 0;
 }
 
@@ -50,12 +52,37 @@ void glu_load(void * into) {
   memcpy(into, gme_board(), GLU_BUF_SIZE);
 }
 
-void g3d_frame(const g3d_frame_api_t * t) {
-  glu_pc.time = tim_now();
+static float glu_aspect_x() {
+  float a = (float)glu_scr_w / (float)glu_scr_h;
+  return a > 1 ? a : 1;
+}
+static float glu_aspect_y() {
+  float a = (float)glu_scr_h / (float)glu_scr_w;
+  return a > 1 ? a : 1;
+}
+void g3d_frame(const g3d_frame_api_t * api) {
+  glu_upc_t pc = {0};
+
+  pc.time = tim_now();
 
   const gme_state_t * gme = gme_state();
-  glu_pc.hover = gme->hover;
-  glu_pc.pick  = gme->pick;
+  pc.hover = gme->hover;
+  pc.pick  = gme->pick;
+
+  float a = (float)glu_scr_w / (float)glu_scr_h;
+  pc.aspect_x = a > 1 ? a : 1;
+  pc.aspect_y = a > 1 ? 1 : (1.0 / a);
+
+  api->load_buffer(glu_upc, &pc, sizeof(glu_upc_t));
+
+  g3d_render_t t = {
+    .ptr      = api->ptr,
+    .pipeline = glu_ppl,
+    .buffers  = (g3d_buffer_t *[]) { glu_upc, 0 },
+    .textures = (g3d_texture_t *[]) { 0 },
+    .samplers = (g3d_sampler_t *[]) { 0 },
+  };
+  api->render(&t);
 }
 
 static float glu_mouse(float p, float a) {
@@ -64,34 +91,22 @@ static float glu_mouse(float p, float a) {
   return p;
 }
 void glu_mouse_move(int x, int y) {
-  float px = glu_mouse((float)x / (float)glu_scr_w, glu_pc.aspect_x);
-  float py = glu_mouse((float)y / (float)glu_scr_h, glu_pc.aspect_y);
-  glu_pc.mouse_x = px;
-  glu_pc.mouse_y = py;
+  float px = glu_mouse((float)x / (float)glu_scr_w, glu_aspect_x());
+  float py = glu_mouse((float)y / (float)glu_scr_h, glu_aspect_y());
+  //glu_pc.mouse_x = px;
+  //glu_pc.mouse_y = py;
   gme_mouse_move(px, py);
 }
 void glu_mouse_down(int x, int y) {
-  float px = glu_mouse((float)x / (float)glu_scr_w, glu_pc.aspect_x);
-  float py = glu_mouse((float)y / (float)glu_scr_h, glu_pc.aspect_y);
-  glu_pc.mouse_x = px;
-  glu_pc.mouse_y = py;
-  gme_mouse_move(px, py);
+  glu_mouse_move(x, y);
   gme_mouse_down();
 }
 void glu_mouse_up(int x, int y) {
-  float px = glu_mouse((float)x / (float)glu_scr_w, glu_pc.aspect_x);
-  float py = glu_mouse((float)y / (float)glu_scr_h, glu_pc.aspect_y);
-  glu_pc.mouse_x = px;
-  glu_pc.mouse_y = py;
-  gme_mouse_move(px, py);
+  glu_mouse_move(x, y);
   gme_mouse_up();
 }
 void glu_mouse_cancel(int x, int y) {
-  float px = glu_mouse((float)x / (float)glu_scr_w, glu_pc.aspect_x);
-  float py = glu_mouse((float)y / (float)glu_scr_h, glu_pc.aspect_y);
-  glu_pc.mouse_x = px;
-  glu_pc.mouse_y = py;
-  gme_mouse_move(px, py);
+  glu_mouse_move(x, y);
   gme_mouse_cancel();
 }
 
