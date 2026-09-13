701c037334f51f0f3b7d87fd8363c8c990908b2e
Eduardo Costa <m4c0@users.noreply.github.com>
2026-09-13 11:25:23 +0100

    gai: preparing to "check" movement

--- gai.h
+++ gai.h
@@ -16,6 +16,16 @@ int gai_tick(const unsigned * board, int side, gai_t * res);
 #include "mve.h"
 #include "opn.h"
 
+typedef struct gai_chk_s {
+} gai_chk_t;
+void gai_chk(const unsigned * board, unsigned p, gai_chk_t * chk) {
+  for (int i = 0; i < 8 * 8; i++) {
+    // TODO: check if under attack
+    // TODO: check if attacks more valueable pieces
+    // TODO: check if attackes more than one piece
+  }
+}
+
 int gai_tick(const unsigned * board, int side, gai_t * res) {
   const opn_mve_t * opn = opn_pick(board);
   if (opn) {
@@ -36,11 +46,16 @@ int gai_tick(const unsigned * board, int side, gai_t * res) {
     for (int j = 0; j < 8 * 8; j++) {
       if (!brd_can_move(board, i, j)) continue;
 
+      // TODO: check if we are going to capture
+
       mve_t mve; mve_new(&mve, board, i, j);
       brd_apply(&mve, brd);
 
       brd_status_t s = brd_status(brd, side);
       if (s != brd_s_normal) continue;
+
+      gai_chk_t chk;
+      gai_chk(board, j, &chk);
 
       unsigned p, n;
       brd_score(brd, &p, &n);
