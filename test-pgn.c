9f08acc1f594ee4d5a65edf710e6458d6699e94e
Eduardo Costa <m4c0@users.noreply.github.com>
2026-09-19 19:41:08 +0100

    pgn: dumping all raw moves

--- test-pgn.c
+++ test-pgn.c
@@ -11,13 +11,87 @@ static int take_round(char ** line) {
   return res;
 }
 
-static int process(char * line) {
-  if (1 != take_round(&line)) {
-    fprintf(stderr, "invalid round: %s", line);
+static inline int is_eom(char c) {
+  return c == 0 || c == '\n' || c == ' ';
+}
+static inline int is_col(char c) {
+  return c >= 'a' && c <= 'h';
+}
+static inline int is_row(char c) {
+  return c >= '1' && c <= '8';
+}
+static inline int is_move(const char * c) {
+  return is_col(c[0]) && is_row(c[1]) && is_eom(c[2]);
+}
+
+static int take_move(char ** line, unsigned * from, unsigned * to, int dir) {
+  char * ptr = *line;
+  if (is_move(ptr)) {
+    printf("pawn to %.2s\n", ptr);
+    *line = ptr + 3;
     return 1;
   }
+  if (is_col(ptr[0]) && ptr[1] == 'x' && is_move(ptr + 2)) {
+    printf("pawn takes %.2s\n", ptr + 2);
+    *line = ptr + 5;
+    return 1;
+  }
+  if (*ptr == 'B' && is_move(ptr + 1)) {
+    printf("bishop to %.2s\n", ptr + 1);
+    *line = ptr + 4;
+    return 1;
+  }
+  if (*ptr == 'B' && ptr[1] == 'x' && is_move(ptr + 2)) {
+    printf("bishop to %.2s\n", ptr + 1);
+    *line = ptr + 5;
+    return 1;
+  }
+  if (*ptr == 'N' && is_move(ptr + 1)) {
+    printf("knight to %.2s\n", ptr + 1);
+    *line = ptr + 4;
+    return 1;
+  }
+  if (*ptr == 'N' && ptr[1] == 'x' && is_move(ptr + 2)) {
+    printf("knight to %.2s\n", ptr + 1);
+    *line = ptr + 5;
+    return 1;
+  }
+  if (*ptr == 'Q' && is_move(ptr + 1)) {
+    printf("queen to %.2s\n", ptr + 1);
+    *line = ptr + 4;
+    return 1;
+  }
+  if (*ptr == 'Q' && ptr[1] == 'x' && is_move(ptr + 2)) {
+    printf("queen to %.2s\n", ptr + 1);
+    *line = ptr + 5;
+    return 1;
+  }
+  if (strncmp(ptr, "O-O", 3) == 0 && is_eom(ptr[3])) {
+    puts("castling");
+    *line = ptr + 4;
+    return 1;
+  }
+  return 0;
+}
 
-  puts(line);
+static int process(char * line) {
+  for (int round = 1; round < 8; round++) {
+    if (round != take_round(&line)) {
+      fprintf(stderr, "invalid round: %s", line);
+      return 1;
+    }
+
+    unsigned from, to;
+    if (!take_move(&line, &from, &to, -1)) {
+      fprintf(stderr, "invalid move: %s", line);
+      return 1;
+    }
+    if (!take_move(&line, &from, &to, 1)) {
+      fprintf(stderr, "invalid move: %s", line);
+      return 1;
+    }
+  }
+  puts("done");
   return 0;
 }
 
