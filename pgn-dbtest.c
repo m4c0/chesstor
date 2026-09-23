38a8b7e69a3cfc1b5a90c67f7aac5986b69bfc5e
Eduardo Costa <m4c0@users.noreply.github.com>
2026-09-23 12:06:07 +0100

    pgn: testing memory db with linked lists

--- pgn-dbtest.c
+++ pgn-dbtest.c
@@ -2,49 +2,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct blk_s {
-  struct blk_s * ptr;
-  unsigned prob;
-} blk_t;
-#define BLOCK_SIZE (sizeof(blk_t) * (8 * 8) * (8 * 8))
+typedef struct node_s {
+  uint16_t ft;
+  uint16_t prob;
+  struct node_s * child;
+  struct node_s * sibling;
+} node_t;
 
-static int block_count = 0;
-static inline void * alloc_block() {
-  block_count++;
-  return calloc(1, BLOCK_SIZE);
+static int node_count = 0;
+static inline void * alloc_node() {
+  node_count++;
+  void * res = malloc(sizeof(node_t));
+  if (!res) fprintf(stderr, "could not allocate node\n");
+  return res;
 }
 
-static int process(blk_t * node, char * line) {
+static int process(node_t * parent, char * line) {
   long from = strtol(line, &line, 16);
   if (from < 0 || from >= (8 * 8)) return (fprintf(stderr, "invalid position: %lx\n", from), 1);
 
   long to = strtol(line, &line, 16);
   if (to < 0 || to >= (8 * 8)) return (fprintf(stderr, "invalid position: %lx\n", to), 1);
 
-  blk_t * n = node + from * (8 * 8) + to;
-  n->prob++;
+  uint16_t ft = from * 64 + to;
+  node_t * n = NULL;
+  if (parent->child) for (n = parent->child; n; n = n->sibling) if (n->ft == ft) break;
+
+  if (n) {
+    if (n->prob < 0xFFFF) n->prob++;
+  } else {
+    n = alloc_node(); if (!n) return 1;
+    *n = (node_t) {
+      .ft      = ft,
+      .prob    = 1,
+      .sibling = parent->child,
+    };
+    parent->child = n;
+  }
 
   if (*line == '\n') return 0;
   if (*line != ' ') return (fprintf(stderr, "invalid post-move char: [%c]\n", *line), 1);
 
-  if (!n->ptr) n->ptr = alloc_block();
-  if (!n->ptr) return (fprintf(stderr, "failed to allocate block\n"), 1);
-
-  return process(n->ptr, line + 1);
+  return process(n, line + 1);
 }
 
 int main() {
   FILE * f = fopen("pgn-extract.out", "rb");
   if (!f) return (fprintf(stderr, "error reading extracted file\n"), 1);
 
-  blk_t * root = alloc_block();
+  node_t * root = alloc_node();
 
+  int ret = 0;
   int count = 0;
   char line[1024];
   while (fgets(line, sizeof(line), f)) {
-    if (process(root, line)) return 1;
+    if (process(root, line)) {
+      ret = 1;
+      break;
+    }
     count++;
   }
-  printf("%d rows, %d blocks in total (%lldmb)\n", count, block_count, (uint64_t)block_count * BLOCK_SIZE / (1024L * 1024L));
+  printf("%d rows, %d nodes in total (%lldmb)\n", count, node_count, (uint64_t)node_count * sizeof(node_t) / (1024L * 1024L));
   fclose(f);
+  return ret;
 }
