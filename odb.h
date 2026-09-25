c88e3e71c3b1344d37c94edf06f18e5d36e4e38a
Eduardo Costa <m4c0@users.noreply.github.com>
2026-09-25 23:14:50 +0100

    odb: preparing to load

--- odb.h
+++ odb.h
@@ -9,15 +9,25 @@ uint32_t odb_pick(uint32_t ofs, const unsigned * board, unsigned * from, unsigne
 
 #ifdef ODB_IMPL
 
-static const void * odb_data;
+static const char * odb_data;
 static unsigned odb_size;
 
 int odb_init(const g3d_api_t * api) {
+  odb_data = api->load_resource("pgn-dbtest", "out", &odb_size);
+
+  if (0 != strncmp(odb_data, "XADREZ00", 8)) odb_data = NULL;
+
   return 0;
 }
 uint32_t odb_reset(void) {
-  return 0;
+  return *(const uint32_t *)(odb_data + odb_size - 4);
 }
+
+typedef struct odb_node_s {
+  uint16_t ft;
+  uint16_t prob;
+  uint32_t ofs;
+} odb_node_t;
 uint32_t odb_pick(uint32_t ofs, const unsigned * board, unsigned * from, unsigned * to) {
   return 0;
 }
